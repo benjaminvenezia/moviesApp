0216c3040732e2c627f5c3cd02148b9b846eda23
Benjamin Venezia <benjamin.venezia@depth-sa.ch>
2025-08-20 19:10:54 +0200

    Implements custom logger and add it to main.go

--- main.go
+++ main.go
@@ -3,15 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/benjaminvenezia/app/logger"
 )
 
+
+func initializeLogger() *logger.Logger {
+
+	loggerInstance, err := logger.NewLogger("movies.log")
+
+	if err != nil {
+		log.Fatalf("Failed to initialize the logger %v", err)
+	}
+
+	defer loggerInstance.Close()
+	return loggerInstance
+}
+
 func main() {
+	logInstance := initializeLogger()
 
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
 	const addr = ":8080"
 	err := http.ListenAndServe(addr, nil)
+	
 	if err != nil {
-		log.Fatalf("Server failed: %v", err)
+		logInstance.Error("Server failed: %v", err)
 	}
 }
